ed41e74c40577f9e5de5f6a84661824af63173c1
johann_bauer <johannsebastianbauer@gmail.com>
2023-11-22 15:14:03 -0300

    ta ai

--- register.php
+++ register.php
@@ -1,2 +1,24 @@
 <?php
 include_once("conexao.php");
+
+$usernameBanco = $_POST["usuario"];
+$senhaBanco = $_POST["password"];
+
+
+//alterando
+
+
+$sql = $conn->prepare("SELECT * FROM usuario WHERE nome_usuario = ?");
+
+ $sql->execute([$usernameBanco]);
+
+
+
+if ($sql->rowCount() == 1) {
+    
+    header("location:index.html");
+}
+
+$sql = $conn->prepare("INSERT INTO usuario(nome_usuario, senha_usuario) VALUES (?,?)");
+
+$sql->execute([$usernameBanco,$senhaBanco]);
